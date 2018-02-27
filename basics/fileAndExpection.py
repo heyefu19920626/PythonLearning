661fafcfef93d6697d4298ba125623042ae59d85
heyefu <527380349@qq.com>
2018-02-27 17:31:13 +0800

    try except else

--- basics/fileAndExpection.py
+++ basics/fileAndExpection.py
@@ -28,5 +28,26 @@ print(ts)
 write_path = 'write.text'
 
 # wirtten to the file
+# w write append
 with open('write_path','a') as file:
 	file.write(ts)
+
+# try expect
+try:
+	print(5/0)
+except Exception:
+	print('You can not divide by zero.')
+
+# try expect else
+print('Give me two numbers,and i will divide them.\nEnter "q" to quit.')
+while True:
+	first_number = input('\nFirst number')
+	if first_number == 'q':
+		break
+	secone_number = input('\nSecond number')
+	try:
+		answer = int(first_number)/int(secone_number)
+	except Exception:
+		print('You can not divide by zero.')
+	else:
+		print(answer)
