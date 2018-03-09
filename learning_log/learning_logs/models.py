ccb0a301f1189a3d2365209a7ace5b4446798039
heyefu <527380349@qq.com>
2018-03-09 17:33:05 +0800

    active model

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Topic(models.Model):
     """ the subject of user learning """
     text = models.CharField(max_length=200)
-    date_added = models.DataTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """ return the string representation of the model """
