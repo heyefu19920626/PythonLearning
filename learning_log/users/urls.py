d020228f704dc836cbafb1e0b473583f46177dd9
heyefu19920626 <527380349@qq.com>
2018-03-13 11:59:50 +0800

    add cancellation user feature

--- learning_log/users/urls.py
+++ learning_log/users/urls.py
@@ -11,4 +11,5 @@ app_name = 'users'
 urlpatterns = [
     # login page
     path('login/', login, {'template_name': 'users/login.html'}, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- learning_log/users/views.py
+++ learning_log/users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import logout
 # Create your views here.
+
+
+def logout_view(request):
+    """ the cancellation of the user """
+    logout(request)
+    #return HttpResponseRedirect(reverse('learning_logs:index'))
+    #return render(request, HttpResponseRedirect(reverse('learning_logs:index')))
+    return render(request, 'learning_logs/index.html')
