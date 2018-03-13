1d452a699427557ec72ae75ea3a43a2391bff346
heyefu19920626 <527380349@qq.com>
2018-03-13 16:45:55 +0800

    restore ForeignKey(keyName,on_delete=models.CASCADE) and migrate database

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -8,7 +8,7 @@ class Topic(models.Model):
     """ the subject of user learning """
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """ return the string representation of the model """
