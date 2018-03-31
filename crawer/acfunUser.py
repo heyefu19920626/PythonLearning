6b79d72efa9c844858d6f8aafc576bbfe6c2892a
heyefu <527380349@qq.com>
2018-04-01 07:05:19 +0800

    add error.log

--- crawer/acfunUser.py
+++ crawer/acfunUser.py
@@ -5,10 +5,12 @@ import json
 # 用来判断是否后面是否还有用户
 is_over = 0
 
+total = 0
 
 def get_user_info(uid, user_json):
     """ 得到用户信息 """
     global is_over
+    global total
     if 'userjson' in user_json.keys():
         user_info = user_json['userjson']
         username = user_info['name']
@@ -21,20 +23,23 @@ def get_user_info(uid, user_json):
             comeFrom = ''
 
         user_info = {'UID': uid, 'name':username, 'regTime': regTime, 'lastLoginIp':lastLoginIp, 'lastLoginDate':lastLoginDate, 'comeFrom': comeFrom}
-        print(user_info)
         with open('acfun_user.json', 'a', encoding='utf-8') as file:
             json.dump(user_info, file, ensure_ascii=False)
+            file.write('\n')
 
+        total += 1
         is_over = 0
     else:
         # print('用户UID:' + str(uid), '用户状态:' + user_json['result'])
         if user_json['result'] == '用户不存在null':
+            with open('error_log.log', 'a', encoding='utf-8') as file:
+                file.write("用户UID:" + str(uid) + "请用户状态:" + user_json['result'] + "\n")
             is_over += 1
 
 
-for i in range(1, 10):
+for i in range(1, 100000000):
     # print(is_over)
-    if is_over > 100:
+    if is_over > 1000:
         break
     url = "http://www.acfun.cn/usercard.aspx?uid=" + str(i)
     response = requests.get(url)
@@ -44,6 +49,11 @@ for i in range(1, 10):
         user_json = json.loads(jsontext)
     except:
         # print("用户UID:" + str(i) + "请求状态:请求页面错误")
+        with open('error_log.log', 'a', encoding='utf-8') as file:
+            file.write("用户UID:" + str(i) + "请求状态:请求页面错误\n")
         continue
 
     get_user_info(i, user_json)
+
+with open('total', 'w', encoding='utf-8') as file:
+    file.wirte('总共有' + str(total) + '有效用户')
