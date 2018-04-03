e5e305f7f1d35b833dd3b7729a177434c359edd4
heyefu <527380349@qq.com>
2018-04-03 21:50:57 +0800

    query from and to station from 12306

--- crawer/ajax_asyn.py
+++ crawer/ajax_asyn.py
@@ -3,10 +3,10 @@ import requests
 import json
 
 
-def get_ticket():
+def get_ticket(from_station, to_station):
     response = requests.get(
         "https://kyfw.12306.cn/otn/leftTicket/queryO?leftTicketDTO.train_date=2018-04-03&leftTicketDTO.from_station"
-        "=JNK&leftTicketDTO.to_station=AOH&purpose_codes=ADULT")
+        "=" + from_station + "&leftTicketDTO.to_station=" + to_station + "&purpose_codes=ADULT")
     print(response.text)
 
 
@@ -16,15 +16,13 @@ def get_ticket():
 # 参数:?leftTicketDTO.train_date=2018-04-03&leftTicketDTO.from_station=JNK&leftTicketDTO.to_station=AOH&purpose_codes=ADULT
 # 日期, 始发站, 终点站, 成人票
 
-# get_ticket()
-
 with open("state_name.txt", encoding="utf-8") as file:
     state_info = file.read()
-state_name_code = {}
+state_dict = {}
 for state_info in state_info.split("@")[1:]:
     state_details = state_info.split("|")
-    state_name_code[state_details[2]] = state_details[1]
-print(state_name_code)
+    state_dict[state_details[1]] = state_details[2]
 with open("state_name_code.json", "a", encoding="utf-8") as file:
-    json.dump(state_name_code, file, ensure_ascii=False)
-print(len(state_name_code))
+    json.dump(state_dict, file, ensure_ascii=False)
+
+get_ticket(state_dict['成都'], state_dict['上海'])
